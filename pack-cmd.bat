7e6054430572c15838f034a92c91fc6c7255ccf0
Microfed <microfed@gmail.com>
2012-02-24 11:36:39 +0400

    - fix pack-cmd.bat

--- pack-cmd.bat
+++ pack-cmd.bat
@@ -5,10 +5,8 @@ set lib-utility-path=%root-dir%lib-utility\
 set yuicompressor-path=%lib-utility-path%yuicompressor-2.4.6.jar
 set pack-path=%root-dir%ready-to-pack\
 set pack-js-path=%pack-path%js
-set pack-css-path=%pack-path%css
 set pack-image-path=%pack-path%i
 set js-path=%root-dir%js
-set css-path=%root-dir%css
 set image-path=%root-dir%i
 
 set zip-utility-path="%programfiles%\7-Zip\"
@@ -18,30 +16,17 @@ SET COPYCMD=/Y
 echo "Copying scripts..."
 xcopy %js-path% %pack-js-path% /i /d /s
 
-echo "Copying css-styles..."
-xcopy %css-path% %pack-css-path% /i /d /s
-
 echo "Copying images..."
 xcopy %image-path% %pack-image-path% /i /d /s
 
 echo "Copying manifest..."
 xcopy %root-dir%manifest.json %pack-path%manifest.json /d
 
-echo "Copying error list..."
-xcopy %root-dir%errorList.json %pack-path%errorList.json /d
-
-echo "Copying html-pages..."
-xcopy %root-dir%background.html %pack-path%background.html /d
-xcopy %root-dir%options.html %pack-path%options.html /d
-
 echo "Compessing and moving a content script..."
-java -jar %yuicompressor-path% -o %pack-js-path%\content-script.js %js-path%\content-script.js
-java -jar %yuicompressor-path% -o %pack-js-path%\DialogBox.js %js-path%\DialogBox.js
-java -jar %yuicompressor-path% -o %pack-js-path%\ErrorListManager.js %js-path%\ErrorListManager.js
-java -jar %yuicompressor-path% -o %pack-js-path%\HabraCorrector.js %js-path%\HabraCorrector.js
-java -jar %yuicompressor-path% -o %pack-js-path%\HabraPage.js %js-path%\HabraPage.js
+java -jar %yuicompressor-path% -o %pack-js-path%\main.js %js-path%\main.js
+java -jar %yuicompressor-path% -o %pack-js-path%\inject.js %js-path%\inject.js
+java -jar %yuicompressor-path% -o %pack-js-path%\collectBonuses.js %js-path%\collectBonuses.js
 java -jar %yuicompressor-path% -o %pack-js-path%\background.js %js-path%\background.js
-java -jar %yuicompressor-path% -o %pack-js-path%\options.js %js-path%\options.js
 
 echo "Compresing files..."
 %zip-utility-path%7z a -r -tzip -o{"%root-dir%"} "pack_extension.zip" ready-to-pack\*
